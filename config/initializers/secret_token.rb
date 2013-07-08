73aa90bddc539b920ca70073f47ac0a72ee0a1e3
ecktows <meddie9@hotmail.com>
2013-07-08 14:57:01 +0100

    Intial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Eckapp::Application.config.secret_key_base = 'eead9e16e6dbff49126ad712813f23061140d5ef92a70f520c49f122168b30d755840f99ba5ed6763baccdd5df80f87d60765cb25a74607c16f125ffa6951e57'
