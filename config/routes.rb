2bf7b6f0965b845e7596b7747f59dc5dc7b84661
Ecktows <Ecktows589@gmail.com>
2013-07-08 16:00:19 +0100

    Added a few static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Eckapp::Application.routes.draw do
   resources :users
 
+  get "static_pages/home"
+  get "static_pages/help"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
